a28acbb12be01e90e6605b8fd659e0f5123d3f39
BharathDevopz <137994267+BharathDevopz@users.noreply.github.com>
2024-11-25 18:44:45 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,43 @@
+variable "gcp_credentials_file" {
+  description = "Path to the GCP credentials JSON file"
+  type        = string
+}
+
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "vm_name" {
+  description = "Name of the VM"
+  type        = string
+}
+
+variable "machine_type" {
+  description = "Machine type (e.g., n1-standard-1)"
+  type        = string
+  default     = "n1-standard-1"
+}
+
+variable "disk_image" {
+  description = "Disk image for the VM (e.g., debian-cloud/debian-11)"
+  type        = string
+}
+
+variable "network" {
+  description = "Network name"
+  type        = string
+  default     = "default"
+}
