e2f7c80ed45cca08fcb694f2f9472c35038f47d7
BharathDevopz <137994267+BharathDevopz@users.noreply.github.com>
2024-11-25 18:44:25 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  credentials = file(var.gcp_credentials_file)
+  project     = var.project_id
+  region      = var.region
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = var.vm_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.disk_image
+    }
+  }
+
+  network_interface {
+    network = var.network
+    access_config {}
+  }
+}
